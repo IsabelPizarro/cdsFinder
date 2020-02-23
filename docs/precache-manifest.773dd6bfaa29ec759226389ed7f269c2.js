ee599d8495fc2e9482bfa0ef09eca57c244bc378
Isabel <chabelipizarro1@gmail.com>
2020-02-23 15:36:51 +0100

    add build and docs

---
+++ docs/precache-manifest.773dd6bfaa29ec759226389ed7f269c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8410f51f97595e1f656a112544ab746d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8fdc10174b00a9723632",
+    "url": "./static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "a19397fcaf01aea17db3",
+    "url": "./static/css/main.bfa621c6.chunk.css"
+  },
+  {
+    "revision": "8fdc10174b00a9723632",
+    "url": "./static/js/2.46633fb2.chunk.js"
+  },
+  {
+    "revision": "a19397fcaf01aea17db3",
+    "url": "./static/js/main.6dac6180.chunk.js"
+  },
+  {
+    "revision": "f10050ce5c840e9a1e4d",
+    "url": "./static/js/runtime-main.8066673c.js"
+  }
+]);
